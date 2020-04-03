0c4d262c50ab68d806e56fce622199ff7c39d2e7
Achintya Kumar <ask60@duke.edu>
2020-04-03 00:57:46 -0400

    commented controller

--- src/ooga/Controller/ControllerInternal.java
+++ src/ooga/Controller/ControllerInternal.java
@@ -3,7 +3,9 @@ package ooga.Controller;
 import java.util.*;
 
 /**
- * 
+ * The controller will take data from the front and back ends and contains
+ * methods that deal with basic game logic, like whether the player
+ * is out of bound and whether the game is over.
  */
 public interface ControllerInternal {
 
