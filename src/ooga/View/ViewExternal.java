ad687b51f054a108c40b37444d8985ba43832b20
Achintya Kumar <ask60@duke.edu>
2020-04-03 00:55:47 -0400

    commented view external

--- src/ooga/View/ViewExternal.java
+++ src/ooga/View/ViewExternal.java
@@ -3,7 +3,8 @@ package ooga.View;
 import java.util.*;
 
 /**
- * 
+ * This will primarily work with the UI and display information to the user. Currently it will have one function
+ * that updates the user score on the interface.
  */
 public interface ViewExternal {
 
