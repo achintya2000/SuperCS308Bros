aa1187ba93954e1863d7c85bd0935ba9fc8146e3
Achintya Kumar <ask60@duke.edu>
2020-04-03 00:32:12 -0400

    fixed viewinternal comments

--- src/ooga/View/ViewInternal.java
+++ src/ooga/View/ViewInternal.java
@@ -3,15 +3,11 @@ package ooga.View;
 import java.util.*;
 
 /**
- * This will primarily be working with user facing activities. It will record things such as what character and
- * state the user picks as well as displaying score and resetting the game.
+ * This will primarily be working getting user facing activities. It will record things such as what character and
+ * state the user picks as well as displaying score and resetting the game and settings bindings so the controller may
+ * pass data to the backend.
  */
 public interface ViewInternal {
-
-    /**
-     * 
-     */
-    public void displayScore();
 
     /**
      * 
