c54cd2868949c3938f1097b37f09226268465475
Achintya Kumar <ask60@duke.edu>
2020-04-03 01:04:12 -0400

    commented modl int

--- src/ooga/Model/ModelInternal.java
+++ src/ooga/Model/ModelInternal.java
@@ -3,7 +3,9 @@ package ooga.Model;
 import java.util.*;
 
 /**
- * 
+ * The model internal class will deal with much of the back end data. It will hand doing actions the backend when it
+ * detects users pressing certain keys. The result of these actions will update certain values that are data bound
+ * to the front end. 
  */
 public interface ModelInternal {
 
