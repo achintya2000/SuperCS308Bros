37402b2747c8cf4f74b2396a1b1ef1a4a66b9dc2
Thomas Chemmanoor <thomas.chemmanoor@duke.edu>
2020-04-03 00:42:40 -0400

    Add new file

---
+++ doc/plan/intersectPlatform.java
@@ -0,0 +1,7 @@
+//Intersect with platform
+public void intersectPlatform(KeyCode e)
+{
+    if(player.getBoundsInParent().intersects(platform.getBoundsInParent())){
+        player.ySpeed = 0;
+    }
+}
