4a38509120f2fa06990b7d8c4e7600fbf07b00c5
Michael Dodd <mmd61@duke.edu>
2020-04-09 22:23:34 -0400

    Created Contoller class to implement interface

---
+++ src/ooga/Controller/Controller.java
@@ -0,0 +1,14 @@
+package ooga.Controller;
+
+public class Controller implements ControllerInternal {
+
+  @Override
+  public void checkWithinBoundries() {
+
+  }
+
+  @Override
+  public void isGameOver() {
+
+  }
+}
