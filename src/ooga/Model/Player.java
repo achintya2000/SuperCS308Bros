99afe7c18d970fc83bd34a0a005773975fee619d
Michael Dodd <mmd61@duke.edu>
2020-04-09 23:19:41 -0400

    Added getter and setters for stocks and stamina

--- src/ooga/Model/Player.java
+++ src/ooga/Model/Player.java
@@ -1,5 +1,38 @@
 package ooga.Model;
 
 public class Player {
+  private int myStocks;
+  private int myStamina;
 
+  /**
+   * Getter for myStamina
+   * @return
+   */
+  public int getStamina() {
+    return myStamina;
+  }
+
+  /**
+   * Setter for myStamina
+   * @param newStamina the new amount of stocks
+   */
+  public void setStamina(int newStamina) {
+    myStamina = newStamina;
+  }
+
+  /**
+   * Getter for myStocks
+   * @return
+   */
+  public int getStocks() {
+    return myStocks;
+  }
+
+  /**
+   * Setter for myStocks
+   * @param newStock the new amount of stocks
+   */
+  public void setStocks(int newStock) {
+    myStocks = newStock;
+  }
 }
