49f06251bdfa46a9f25e2098382f44b79b5949f7
Achintya Kumar <ask60@duke.edu>
2020-04-11 00:50:45 -0400

    testing moving key info to Character class

---
+++ src/ooga/Model/Characters/Character1.java
@@ -0,0 +1,66 @@
+package ooga.Model.Characters;
+
+import javafx.animation.Animation;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+import ooga.Model.GameEngine.SpriteAnimation;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Character1 {
+
+    private static final int COLUMNS  =   10;
+    private static final int COUNT    =  10;
+    private static final int OFFSET_X =  0;
+    private static final int OFFSET_Y =  0;
+    private static final int WIDTH    = 366;
+    private static final int HEIGHT   = 461;
+
+    Image RUN_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/run.png"));
+    Image IDLE_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/idle.png"));
+    Image ATTACK_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/attack.png"));
+
+    ImageView spriteImageView;
+    SpriteAnimation spriteAnimation;
+
+    public Character1() throws FileNotFoundException {
+        spriteImageView = new ImageView(IDLE_IMAGE);
+        spriteImageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        playIdleAnimation();
+    }
+
+    public void moveRight() {
+        playRunRightAnimation();
+        spriteImageView.setX(spriteImageView.getX() + 30);
+    }
+
+    public ImageView getCharacterImage(){
+        return spriteImageView;
+    }
+
+    private void playIdleAnimation() {
+        spriteAnimation = new SpriteAnimation(
+                spriteImageView,
+                Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                234, 442
+        );
+        spriteAnimation.setCycleCount(Animation.INDEFINITE);
+        spriteAnimation.play();
+    }
+
+    private void playRunRightAnimation() {
+        spriteImageView.setImage(RUN_IMAGE);
+        spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT);
+        spriteAnimation.play();
+    }
+
+
+}
--- src/ooga/Model/GameEngine/SpriteTester.java
+++ src/ooga/Model/GameEngine/SpriteTester.java
@@ -12,45 +12,49 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import ooga.Model.Characters.Character1;
 import ooga.Model.GameEngine.SpriteAnimation;
 
 import java.io.FileInputStream;
 
 public class SpriteTester extends Application {
 
-    private static final int COLUMNS  =   10;
-    private static final int COUNT    =  10;
-    private static final int OFFSET_X =  0;
-    private static final int OFFSET_Y =  0;
-    private static final int WIDTH    = 366;
-    private static final int HEIGHT   = 461;
+//    private static final int COLUMNS  =   10;
+//    private static final int COUNT    =  10;
+//    private static final int OFFSET_X =  0;
+//    private static final int OFFSET_Y =  0;
+//    private static final int WIDTH    = 366;
+//    private static final int HEIGHT   = 461;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Creating an image
-        Image RUN_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/run.png"));
-        Image IDLE_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/idle.png"));
-        Image ATTACK_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/attack.png"));
 
-        //Setting the image view
-        ImageView spriteImageView = new ImageView(IDLE_IMAGE);
-        spriteImageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        Character1 ninja = new Character1();
+
+//        //Creating an image
+//        Image RUN_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/run.png"));
+//        Image IDLE_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/idle.png"));
+//        Image ATTACK_IMAGE = new Image(new FileInputStream("resources/spritesheets/chracter1/attack.png"));
+//
+//        //Setting the image view
+//        ImageView spriteImageView = new ImageView(IDLE_IMAGE);
+//        spriteImageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
 
         //spriteImageView.setPreserveRatio(true);
         //spriteImageView.setFitHeight(100);
         //imageView.setFitWidth(100);
 
-        SpriteAnimation spriteAnimation = new SpriteAnimation(
-                spriteImageView,
-                Duration.millis(1000),
-                COUNT, COLUMNS,
-                OFFSET_X, OFFSET_Y,
-                234, 442
-        );
-        spriteAnimation.setCycleCount(Animation.INDEFINITE);
-        spriteAnimation.play();
-        
-        Group root = new Group(spriteImageView);
+//        SpriteAnimation spriteAnimation = new SpriteAnimation(
+//                spriteImageView,
+//                Duration.millis(1000),
+//                COUNT, COLUMNS,
+//                OFFSET_X, OFFSET_Y,
+//                234, 442
+//        );
+//        spriteAnimation.setCycleCount(Animation.INDEFINITE);
+//        spriteAnimation.play();
+
+        Group root = new Group(ninja.getCharacterImage());
 
         //Creating a scene object
         Scene scene = new Scene(root, 1000, 1000);
@@ -66,50 +70,49 @@ public class SpriteTester extends Application {
 
         scene.setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.D) {
-                spriteImageView.setImage(RUN_IMAGE);
-                //spriteAnimation.stop();
-                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
-                        COUNT, COLUMNS,
-                        OFFSET_X, OFFSET_Y,
-                        WIDTH, HEIGHT);
-                spriteAnimation.play();
-
-                spriteImageView.setX(spriteImageView.getX() + 30);
+                ninja.moveRight();
+//                spriteImageView.setImage(RUN_IMAGE);
+//                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
+//                        COUNT, COLUMNS,
+//                        OFFSET_X, OFFSET_Y,
+//                        WIDTH, HEIGHT);
+//                spriteAnimation.play();
+//                spriteImageView.setX(spriteImageView.getX() + 30);
             }
             if (e.getCode() == KeyCode.T) {
-                spriteAnimation.stop();
-                spriteImageView.setImage(ATTACK_IMAGE);
-                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
-                        COUNT, COLUMNS,
-                        OFFSET_X, OFFSET_Y,
-                        536, 495);
-                spriteAnimation.setCycleCount(1);
-                spriteAnimation.play();
-
-                spriteAnimation.setOnFinished(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        spriteAnimation.stop();
-                        spriteImageView.setImage(IDLE_IMAGE);
-                        spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
-                                COUNT, COLUMNS,
-                                OFFSET_X, OFFSET_Y,
-                                234, 442);
-                        spriteAnimation.setCycleCount(Animation.INDEFINITE);
-                        spriteAnimation.play();
-                    }
-                });
+//                spriteAnimation.stop();
+//                spriteImageView.setImage(ATTACK_IMAGE);
+//                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
+//                        COUNT, COLUMNS,
+//                        OFFSET_X, OFFSET_Y,
+//                        536, 495);
+//                spriteAnimation.setCycleCount(1);
+//                spriteAnimation.play();
+//
+//                spriteAnimation.setOnFinished(new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        spriteAnimation.stop();
+//                        spriteImageView.setImage(IDLE_IMAGE);
+//                        spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
+//                                COUNT, COLUMNS,
+//                                OFFSET_X, OFFSET_Y,
+//                                234, 442);
+//                        spriteAnimation.setCycleCount(Animation.INDEFINITE);
+//                        spriteAnimation.play();
+//                    }
+//                });
             }
         });
 
         scene.setOnKeyReleased(e -> {
             if (e.getCode() == KeyCode.D) {
-                spriteImageView.setImage(IDLE_IMAGE);
-                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
-                        COUNT, COLUMNS,
-                        OFFSET_X, OFFSET_Y,
-                        234, 442);
-                spriteAnimation.play();
+//                spriteImageView.setImage(IDLE_IMAGE);
+//                spriteAnimation.setAnimation(spriteImageView, Duration.millis(1000),
+//                        COUNT, COLUMNS,
+//                        OFFSET_X, OFFSET_Y,
+//                        234, 442);
+//                spriteAnimation.play();
             }
             if (e.getCode() == KeyCode.T){
                 //if (spriteAnimation.)
