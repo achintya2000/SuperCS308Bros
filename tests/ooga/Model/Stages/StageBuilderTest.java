d2b3a06afbf3e93cfa9c80cd730fa75d50e47b5c
Michael Dodd <mmd61@duke.edu>
2020-04-19 18:48:31 -0400

    Added things to StageBuilder tests

--- tests/ooga/Model/Stages/StageBuilderTest.java
+++ tests/ooga/Model/Stages/StageBuilderTest.java
@@ -2,15 +2,20 @@ package ooga.Model.Stages;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class StageBuilderTest {
 
   @Test
   void makePlatforms() {
+    assertEquals(0, 0);
   }
 
   @Test
   void getPlatforms() {
+    ArrayList<Rectangle> thing = null;
+    assertNotEquals(thing, 0);
   }
 }
