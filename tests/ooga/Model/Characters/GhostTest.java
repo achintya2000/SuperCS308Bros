efa3ddb4b5974ca65efe8824ff6be7c5e3355cea
Michael Dodd <mmd61@duke.edu>
2020-04-19 18:45:29 -0400

    Created more test files

---
+++ tests/ooga/Model/Characters/GhostTest.java
@@ -0,0 +1,64 @@
+package ooga.Model.Characters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GhostTest {
+
+  @Test
+  void idle() {
+  }
+
+  @Test
+  void moveLeft() {
+  }
+
+  @Test
+  void moveRight() {
+  }
+
+  @Test
+  void moveDown() {
+  }
+
+  @Test
+  void jump() {
+  }
+
+  @Test
+  void attack() {
+  }
+
+  @Test
+  void special() {
+  }
+
+  @Test
+  void getRoot() {
+  }
+
+  @Test
+  void getCharacterImage() {
+  }
+
+  @Test
+  void getCenterY() {
+  }
+
+  @Test
+  void setCenterY() {
+  }
+
+  @Test
+  void getHitBox() {
+  }
+
+  @Test
+  void getHurtBox() {
+  }
+
+  @Test
+  void setImageFiles() {
+  }
+}
---
+++ tests/ooga/Model/Stages/StageBuilderTest.java
@@ -0,0 +1,16 @@
+package ooga.Model.Stages;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StageBuilderTest {
+
+  @Test
+  void makePlatforms() {
+  }
+
+  @Test
+  void getPlatforms() {
+  }
+}
