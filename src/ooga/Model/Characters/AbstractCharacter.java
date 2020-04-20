6473f3772be604769aadf631114e936dcacdd3b3
Achintya Kumar <ask60@duke.edu>
2020-04-19 20:49:13 -0400

    health bars go down

--- src/ooga/Model/Characters/AbstractCharacter.java
+++ src/ooga/Model/Characters/AbstractCharacter.java
@@ -67,10 +67,10 @@ public abstract class AbstractCharacter {
   public SimpleDoubleProperty healthProperty(){
     return HEALTH;
   }
+
   public void setHEALTH(double new_health){
     HEALTH.set(new_health);
   }
-
   public double getHEALTH(){
     return HEALTH.get();
   }
--- src/ooga/View/GameView.java
+++ src/ooga/View/GameView.java
@@ -208,6 +208,7 @@ public class GameView extends Application implements ViewInternal {
             if (bunny.getHitBox().getBoundsInParent()
                     .intersects(bunny2.getHurtBox().getBoundsInParent())) {
               bunny2.getHurtBox().setStroke(Color.RED);
+              bunny2.setHEALTH(bunny2.getHEALTH() - 10);
             }
           }
         } catch (IllegalAccessException | NoSuchFieldException ex) {
@@ -248,6 +249,7 @@ public class GameView extends Application implements ViewInternal {
             if (bunny2.getHitBox().getBoundsInParent()
                     .intersects(bunny.getHurtBox().getBoundsInParent())) {
               bunny.getHurtBox().setStroke(Color.RED);
+              bunny.setHEALTH(bunny.getHEALTH() - 10);
             }
           }
         } catch (IllegalAccessException | NoSuchFieldException ex) {
