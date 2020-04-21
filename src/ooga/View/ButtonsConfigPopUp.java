d532ece9b4f173c74422f5ed13f5419eed4a7ec5
Thomas Chemmanoor <tpc14@duke.edu>
2020-04-21 00:53:27 -0400

    Added ability to access settings from stage select. Fixed display errors for json buttons.

--- src/ooga/View/ButtonsConfigPopUp.java
+++ src/ooga/View/ButtonsConfigPopUp.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -29,22 +30,25 @@ public class ButtonsConfigPopUp {
 
     BorderPane borderPane = new BorderPane();
     Label settingsText= new Label("Button Configurations");
+    HBox topElements = new HBox();
+    topElements.setAlignment(Pos.CENTER);
+    topElements.getChildren().add(settingsText);
 
 
     VBox p1Config= new VBox(10);
     TextField leftButton1 = new TextField(buttonConfigurer.getPlayer1LeftKey());
     TextField rightButton1 = new TextField(buttonConfigurer.getPlayer1RightKey());
     TextField jumpButton1 = new TextField(buttonConfigurer.getPlayer1JumpKey());
-    TextField fallButton1 = new TextField(buttonConfigurer.getPlayer1LeftKey());
-    TextField attackButton1 = new TextField(buttonConfigurer.getPlayer1LeftKey());
+    TextField fallButton1 = new TextField(buttonConfigurer.getPlayer1FallKey());
+    TextField attackButton1 = new TextField(buttonConfigurer.getPlayer1AttackKey());
     TextField specialButton1 = new TextField(buttonConfigurer.getPlayer1SpecialKey());
 
     VBox p2Config= new VBox(10);
     TextField leftButton2 = new TextField(buttonConfigurer.getPlayer2LeftKey());
     TextField rightButton2 = new TextField(buttonConfigurer.getPlayer2RightKey());
     TextField jumpButton2 = new TextField(buttonConfigurer.getPlayer2JumpKey());
-    TextField fallButton2 = new TextField(buttonConfigurer.getPlayer2LeftKey());
-    TextField attackButton2 = new TextField(buttonConfigurer.getPlayer2LeftKey());
+    TextField fallButton2 = new TextField(buttonConfigurer.getPlayer2FallKey());
+    TextField attackButton2 = new TextField(buttonConfigurer.getPlayer2AttackKey());
     TextField specialButton2 = new TextField(buttonConfigurer.getPlayer2SpecialKey());
 
     p1Config.getChildren().addAll(leftButton1, rightButton1, jumpButton1, fallButton1,attackButton1,specialButton1);
@@ -57,10 +61,12 @@ public class ButtonsConfigPopUp {
       buttonConfigurer.setPlayer2KeyBinds(leftButton2.getText(),rightButton2.getText(), jumpButton2.getText(), fallButton2.getText(),attackButton2.getText(),specialButton2.getText());
       buttonConfigsStage.close();
     });
+    HBox bottomElements = new HBox();
+    bottomElements.setAlignment(Pos.CENTER);
+    bottomElements.getChildren().add(configure);
 
-
-    borderPane.setTop(settingsText);
-    borderPane.setBottom(configure);
+    borderPane.setTop(topElements);
+    borderPane.setBottom(bottomElements);
     borderPane.setLeft(p1Config);
     borderPane.setRight(p2Config);
     Scene settingsScene= new Scene(borderPane, 500, 400);
