df5f867baa7b2cd5443e1e4c966caa82c65629d7
Michael Dodd <mmd61@duke.edu>
2020-04-23 16:40:55 -0400

    Worked with achintya on networking

--- src/ooga/networking/ClientNetworkListener.java
+++ src/ooga/networking/ClientNetworkListener.java
@@ -6,35 +6,35 @@ import com.esotericsoftware.kryonet.Listener;
 
 public class ClientNetworkListener extends Listener {
 
-    private Client client;
+  private Client client;
 
-    public void init(Client c) {
-        this.client = c;
+  public void init(Client c) {
+    this.client = c;
+  }
+
+  @Override
+  public void connected(Connection c) {
+    System.out.println("[CLIENT] >> You have connect");
+
+    // Create something to sent
+    Packet.packet01Message message = new Packet.packet01Message();
+    message.message = "Hello server. YEETICUS";
+
+    client.sendTCP(message);
+  }
+
+  @Override
+  public void disconnected(Connection c) {
+    System.out.println("[CLIENT] >> You have disconnect");
+  }
+
+  @Override
+  public void received(Connection c, Object o) {
+    if (o instanceof Packet.packet01Message) {
+      Packet.packet01Message packet01Message = (Packet.packet01Message) o;
+
+      System.out.println("[SERVER} >> " + packet01Message.message);
     }
-
-    @Override
-    public void connected(Connection c) {
-        System.out.println("[CLIENT] >> You have connect");
-
-        // Create something to sent
-        Packet.packet01Message message = new Packet.packet01Message();
-        message.message = "Hello server. YEETICUS";
-
-        client.sendTCP(message);
-    }
-
-    @Override
-    public void disconnected(Connection c) {
-        System.out.println("[CLIENT] >> You have disconnect");
-    }
-
-    @Override
-    public void received(Connection c, Object o) {
-        if (o instanceof Packet.packet01Message) {
-            Packet.packet01Message packet01Message = (Packet.packet01Message) o;
-
-            System.out.println("[SERVER} >> " + packet01Message.message);
-        }
-    }
+  }
 
 }
--- src/ooga/networking/MPServer.java
+++ src/ooga/networking/MPServer.java
@@ -10,40 +10,40 @@ import java.io.IOException;
 
 public class MPServer {
 
-    // Server
-    private Server server;
+  // Server
+  private Server server;
 
-    // Connection details
-    private String IPConnection = "localhost";
-    private int ServerPort = 2300;
+  // Connection details
+  private String IPConnection = "localhost";
+  private int ServerPort = 2300;
 
-    // Listener
-    private ServerNetworkListener serverNetworkListener;
+  // Listener
+  private ServerNetworkListener serverNetworkListener;
 
-    public MPServer() {
-        server = new Server();
-        serverNetworkListener = new ServerNetworkListener();
+  public MPServer() {
+    server = new Server();
+    serverNetworkListener = new ServerNetworkListener();
 
-        server.addListener(serverNetworkListener);
+    server.addListener(serverNetworkListener);
 
-        try {
-            server.bind(ServerPort);
-        } catch (IOException e) {
-            new ExceptionHelper(e);
-        }
-
-        registerPackets();
-
-        server.start();
+    try {
+      server.bind(ServerPort, 2301);
+    } catch (IOException e) {
+      new ExceptionHelper(e);
     }
 
-    private void registerPackets() {
-        Kryo kryo = server.getKryo();
-        kryo.register(Packet.packet01Message.class);
-    }
+    registerPackets();
+
+    server.start();
+  }
+
+  private void registerPackets() {
+    Kryo kryo = server.getKryo();
+    kryo.register(Packet.packet01Message.class);
+  }
 
 
-    public static void main(String[] args) {
-        new MPServer();
-    }
+  public static void main(String[] args) {
+    new MPServer();
+  }
 }
