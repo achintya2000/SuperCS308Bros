59dd866568f1cd90818a27e617c8b4df37770646
Thomas Chemmanoor <tpc14@duke.edu>
2020-04-24 17:57:02 -0400

    Refactored Home class

--- src/ooga/View/Home.java
+++ src/ooga/View/Home.java
@@ -1,37 +1,29 @@
 package ooga.View;
 
-
-import java.awt.MouseInfo;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
-import javax.sound.midi.SysexMessage;
 import ooga.Controller.MusicManager;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.ResourceBundle;
 
-import static javafx.geometry.Pos.BOTTOM_CENTER;
-import static javafx.geometry.Pos.CENTER;
 
 public class Home extends Application {
 
   private Stage currentStage;
+  private BorderPane borderPane;
+  private HBox buttonHolder;
+  private Button playLocal;
+  private Button playOnline;
 
   @Override
   public void start(Stage primaryStage) {
     System.out.println();
-    Scene homeScreen = new Scene(makeGridPane());
+    Scene homeScreen = new Scene(setupBorderPane());
     primaryStage.setScene(homeScreen);
     primaryStage.setHeight(800);
     primaryStage.setWidth(1200);
@@ -42,27 +34,23 @@ public class Home extends Application {
     //musicManager.playMainMenuMusic();
   }
 
-  private BorderPane makeGridPane() {
-    BorderPane myGP = new BorderPane();
+  private BorderPane setupBorderPane() {
+    borderPane = new BorderPane();
     BackgroundImage homeScreen = new BackgroundImage(
         new Image("SmashHomeScreen.jpg", 1200, 800, false, true),
         BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
         BackgroundSize.DEFAULT);
-    myGP.setBackground(new Background(homeScreen));
+    borderPane.setBackground(new Background(homeScreen));
 
-    Button playLocal = new Button();
-    playLocal.setAlignment(Pos.BOTTOM_LEFT);
-    playLocal.setGraphic(new ImageView(new Image("LocalSmash.png", 575, 175, false, true)));
+    createButtons();
+    setButtonActions();
 
-    Button playOnline = new Button();
-    playOnline.setAlignment(Pos.BOTTOM_RIGHT);
+    borderPane.setCenter(buttonHolder);
 
-    playOnline.setGraphic(new ImageView(new Image("OnlineSmash.png", 575, 175, false, true)));
+    return borderPane;
+  }
 
-    HBox buttonHolder = new HBox();
-    buttonHolder.getChildren().addAll(playLocal,playOnline);
-    buttonHolder.setAlignment(BOTTOM_CENTER);
-
+  private void setButtonActions() {
     playLocal.setOnAction(e -> {
       try {
         localPlay();
@@ -71,10 +59,19 @@ public class Home extends Application {
       }
     });
     playOnline.setOnAction(e -> onlinePlay());
+  }
 
-    myGP.setCenter(buttonHolder);
+  private void createButtons() {
+    playLocal = new Button();
+    playLocal.setAlignment(Pos.BOTTOM_LEFT);
+    playLocal.setGraphic(new ImageView(new Image("LocalSmash.png", 575, 175, false, true)));
+    playOnline = new Button();
+    playOnline.setAlignment(Pos.BOTTOM_RIGHT);
+    playOnline.setGraphic(new ImageView(new Image("OnlineSmash.png", 575, 175, false, true)));
 
-    return myGP;
+    buttonHolder = new HBox();
+    buttonHolder.getChildren().addAll(playLocal,playOnline);
+    buttonHolder.setAlignment(Pos.BOTTOM_CENTER);
   }
 
 
