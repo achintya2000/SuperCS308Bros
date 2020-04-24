fe24fcd1fe28745a56e14472d0747e999b82efa7
Thomas Chemmanoor <tpc14@duke.edu>
2020-04-24 00:49:32 -0400

    Added play local nad play online buttons to home screen

--- src/ooga/View/Home.java
+++ src/ooga/View/Home.java
@@ -6,7 +6,9 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -16,12 +18,16 @@ import javax.sound.midi.SysexMessage;
 import ooga.Controller.MusicManager;
 
 import java.io.File;
+import java.util.ResourceBundle;
 
+import static javafx.geometry.Pos.BOTTOM_CENTER;
 import static javafx.geometry.Pos.CENTER;
 
 public class Home extends Application {
 
   private Stage currentStage;
+  public static final ResourceBundle buttonStyles = ResourceBundle
+          .getBundle("ooga.Resources.stylesheets.buttonStyle");
 
   @Override
   public void start(Stage primaryStage) {
@@ -45,14 +51,20 @@ public class Home extends Application {
     myGP.setBackground(new Background(homeScreen));
     myGP.setOnMouseClicked(e -> play());
 
-    Button playLocal = new Button("PLAY LOCAL");
-    playLocal.setAlignment(Pos.CENTER_LEFT);
-    Button playOnline = new Button("PLAY ONLINE");
-    playLocal.setAlignment(Pos.CENTER_RIGHT);
+
+
+    Button playLocal = new Button();
+    playLocal.setAlignment(Pos.BOTTOM_LEFT);
+    playLocal.setGraphic(new ImageView(new Image("LocalSmash.png", 575, 175, false, true)));
+
+    Button playOnline = new Button();
+    playOnline.setAlignment(Pos.BOTTOM_RIGHT);
+
+    playOnline.setGraphic(new ImageView(new Image("OnlineSmash.png", 575, 175, false, true)));
 
     HBox buttonHolder = new HBox();
     buttonHolder.getChildren().addAll(playLocal,playOnline);
-    buttonHolder.setAlignment(CENTER);
+    buttonHolder.setAlignment(BOTTOM_CENTER);
 
     myGP.setCenter(buttonHolder);
 
